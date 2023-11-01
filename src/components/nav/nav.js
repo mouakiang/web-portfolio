f221b9a8337bca3c24f1aa72b4ca9c267abd27f2
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-11-01 16:54:52 -0400

    added links to nav and styling

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./nav.css";
 import logo from "../../assets/logo-s.png";
+import { Link } from "react-scroll";
+
 
 const nav = () => {
   return (
     <nav className="navbar">
       <img src={logo} alt="logo" className="logo" />
-      <img src="" alt="" />
-      <div className="desktop-menu"></div>
+      <div className="desktop-menu">
+        <Link className="nav-link">Home</Link>
+        <Link className="nav-link">About</Link>
+        <Link className="nav-link">Projects</Link>
+        <Link className="nav-link">Contact</Link>
+      </div>
     </nav>
   );
 };
