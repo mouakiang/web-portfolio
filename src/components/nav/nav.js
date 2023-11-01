7f8eca0b11817ba369b4b8b064a9a45d13cfc11e
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-11-01 17:04:15 -0400

    added styling to fontAwesomeIcons

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -2,17 +2,31 @@ import React from "react";
 import "./nav.css";
 import logo from "../../assets/logo-s.png";
 import { Link } from "react-scroll";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUser,
+  faLaptop,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 
 const nav = () => {
   return (
     <nav className="navbar">
       <img src={logo} alt="logo" className="logo" />
       <div className="desktop-menu">
-        <Link className="nav-link">Home</Link>
-        <Link className="nav-link">About</Link>
-        <Link className="nav-link">Projects</Link>
-        <Link className="nav-link">Contact</Link>
+        <Link className="nav-link">
+          <FontAwesomeIcon icon={faHouse} className="fa-2x" />
+        </Link>
+        <Link className="nav-link">
+          <FontAwesomeIcon icon={faUser} className="fa-2x" />
+        </Link>
+        <Link className="nav-link">
+          <FontAwesomeIcon icon={faLaptop} className="fa-2x" />
+        </Link>
+        <Link className="nav-link">
+          <FontAwesomeIcon icon={faEnvelope} className="fa-2x" />
+        </Link>
       </div>
     </nav>
   );
