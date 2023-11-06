cecabb92978f47cbf4e13483d9939aff01c70c40
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-11-06 16:21:41 -0500

    divs added for about me bars as well as images and text

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,13 +1,45 @@
 import React from "react";
-import './about.css';
+import "./about.css";
+import feimg from "../../assets/front-end development.png"
+import beimg from "../../assets/back-end development.png";
+import fsimg from "../../assets/full-stack development.png";
 
 const about = () => {
-
-    return(
-        <section id="skills">
-           <h2>About Section</h2>
-        </section>
-    );
-}
+  return (
+    <section id="about">
+      <span className="about-title">What I Do</span>
+      <span className="about-info">
+        I am a passionate Full-Stack Software Engineer, skilled in JavaScript,
+        React, Node.js, and more, with a love for crafting both front-end and
+        back-end solutions.
+      </span>
+      <div className="about-bars">
+        <div className="about-single-bar">
+            <img src={fsimg} alt="full-stack" className="about-bar-img"/>
+            <div className="about-bar-text">
+                <h2>Full-Stack Development</h2>
+                <p>Both Front-End and Back-End Development with additional skills such as:
+                    MOCHA || CHAI || KNEX || GIT || GITHUB || ADOBE PHOTOSHOP || ADOBE LIGHTROOM
+                </p>
+            </div>
+        </div>
+        <div className="about-single-bar">
+            <img src={feimg} alt="front-end" className="about-bar-img"/>
+            <div className="about-bar-text">
+                <h2>Front-End Development</h2>
+                <p>TECH STACK: HTML || CSS || JAVASCRIPT || REACT.JS || JQUERY || BOOTSTRAP</p>
+            </div>
+        </div>
+        <div className="about-single-bar">
+            <img src={beimg} alt="back-end" className="about-bar-img"/>
+            <div className="about-bar-text">
+                <h2>Back-End Development</h2>
+                <p>TECH STACK: NODE.JS || EXPRESS || RESTFUL APIS || POSTGRESQL</p>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default about;
