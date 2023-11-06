6bb41e463016f1aeafe938008baf209df4f9165f
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-11-06 15:03:58 -0500

    about component created and routed in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Nav from './components/nav/nav';
 import Home from './components/home/home';
+import About from './components/about/about';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './about.css';
+
+const about = () => {
+
+    return(
+        <section id="skills">
+           <h2>About Section</h2>
+        </section>
+    );
+}
+
+export default about;
