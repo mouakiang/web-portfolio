23005cc1d5ef0aa0ab3fb9f8a6febcd0b07ec27a
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-11-06 14:20:37 -0500

    added background image to home content

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,27 +1,26 @@
 import React from "react";
 import "./home.css";
-import bg from "../../assets/coding-head.png";
 import resume from "../../assets/resume.pdf";
+import bg from "../../assets/computer.png";
 
 const home = () => {
   return (
     <section id="home">
-      <div className="home-content">
+      <div className="home-content image">
         <span className="hello">Hello, </span>
         <span className="intro-text">
           I'm <span className="intro-name">Kia,</span>
           <br />
-         <span className="developer">Web Developer</span> 
+          <span className="developer">Software Engineer</span>
         </span>
         <p className="intro-p">
-          I am a <b>Web Developer</b> with experience in building Full-Stack Web
-          Applications focusing on <i>JavaScript </i>and <i>React.js.</i>
+          I am a <b>Software Engineer</b> with experience in building Full-Stack
+          Web Applications focusing on <i>JavaScript </i>and <i>React.js.</i>
         </p>
         <a className="btn btn-success" href={resume} download="resume.pdf">
           Resume
         </a>
       </div>
-      <img src={bg} alt="coding head" className="bg" />
     </section>
   );
 };
