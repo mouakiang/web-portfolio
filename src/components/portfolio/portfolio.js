1cfe06e9623bc3b8911c933be6728e6afd0fde8e
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-11-08 15:31:40 -0500

    added portfolio content and a bit of styling

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './portfolio.css';
+import React from "react";
+import "./portfolio.css";
+import restres from "../../assets/restaurant-reservation.png";
+import movies from "../../assets/we-love-movies.png";
 
 const portfolio = () => {
-    return (
-        <div>
-
-        </div>
-    )
-}
+  return (
+    <section id="portfolio">
+      <h1 className="myportfolio">My Portfolio</h1>
+      <span className="portfolio-info">
+        My portfolio showcases a progression of projects where I've applied a
+        blend of front-end and back-end technologies to solve complex problems.
+        From designing responsive user interfaces to architecting server-side
+        solutions, each project underscores my commitment to building scalable,
+        efficient, and user-centric software. Dive into my work to see how I've
+        leveraged JavaScript, React, Node.js, and a suite of other technologies
+        to deliver seamless digital experiences.
+      </span>
+      <div className="portfolio-images">
+        <img
+          src={restres}
+          alt="restaurant reservation"
+          className="portfolio-img-one"
+        />
+        <img src={movies} alt="we love movies" className="portfolio-img-two" />
+      </div>
+    </section>
+  );
+};
 
 export default portfolio;
